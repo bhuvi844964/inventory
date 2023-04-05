cd87dd82af26362936fe7263e52be240cc8cb5c9
bhuvnesh <bhuvi844964@gmail.com>
2023-04-05 10:19:07 +0530

    initial commit

--- Inventory server/index.js
+++ Inventory server/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require("cors");
 require("./db/confige")
+require('dotenv').config()
 const route = require("./routes/route")
 const app = express()
 
